1274ea5373d1b1e2c5f61c1105bcc53d719b75f2
Kessler <ak113714@bna.com>
2020-05-22 11:48:57 -0400

    added plot of natl weekly

--- 01_analysis.R
+++ 01_analysis.R
@@ -7,13 +7,24 @@ options(scipen = 999)
 source("00_load_data.R")
 
 
-# we'll start with the national weekly file 
+# we'll start with the national weekly file - note that week of april 11 is the peak the SDI index measured against
 raw_national_weekly <- read_csv("raw_data/SD_national_scaled_weekly.csv")
 
 #format and clean names
 natl_weekly <- raw_national_weekly %>% 
   clean_names() %>% 
+  rename(sdindex = s_dindex) %>% #clean_names struggled with this one, so manual fix
   mutate(
     time = dmy_hms(time),
     time = date(time)
   )
+
+natl_weekly
+
+#plot
+natl_weekly %>% 
+  ggplot(aes(x = time,
+             y = sdindex)) +
+  geom_col() +
+  theme_minimal()
+
